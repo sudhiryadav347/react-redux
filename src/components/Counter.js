ef0be65a823537f306f6d099cbdb5bef4375d9fd
Sudhir <sudhir@livelgam.com>
2022-03-18 23:22:27 +0530

    #2 migrate to redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,44 +1,54 @@
-import React, { useRef } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import React , { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
 
 const Counter = () => {
 
     const dispatch = useDispatch();
-    const counter = useSelector((state) => state.counter);
-    const counterDisplay = useSelector((state) => state.show);
+    const counter = useSelector((state) => state.counter.counter);
+    const counterDisplay = useSelector((state) => state.counter.show);
     const inputAmountRef = useRef();
 
     const incrementHandler = () => {
-        dispatch({
-            type: 'increment'
-        });
+        // dispatch({
+        //     type: 'increment'
+        // });
+        dispatch(counterActions.increment());
     };
     const increaseHandler = () => {
-        dispatch({
-            type: 'increase',
+        dispatch(counterActions.increase({
             amount: +inputAmountRef.current.value
-        });
+        }));
+        inputAmountRef.current.value = 0;
+
+
+        // dispatch({
+        //     type: 'increase',
+        //     amount: +inputAmountRef.current.value
+        // });
     };
 
     const decrementHandler = () => {
-        dispatch({
-            type: 'decrement'
-        });
+        // dispatch({
+        //     type: 'decrement'
+        // });
+        dispatch(counterActions.decrement());
     };
     const toggleCounterHandler = () => {
-        dispatch({
-            type: 'visibility'
-        });
+        // dispatch({
+        //     type: 'visibility'
+        // });
+        dispatch(counterActions.visibility());
     };
     const resetHandler = () => {
-        dispatch({
-            type: 'reset'
-        });
+        // dispatch({
+        //     type: 'reset'
+        // });
+        dispatch(counterActions.reset());
         inputAmountRef.current.value = 0;
     }
 
-    console.log(counterDisplay);
-    console.log(counter)
 
     return (
         <div>
