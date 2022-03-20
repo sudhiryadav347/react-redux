b2157e61c97f875275ba13180855470c86fee11d
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-03-20 09:26:12 +0530

    #4 Dealing with multiple slices

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import Counter  from './components/Counter';
+import Counter from './components/Counter';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <Counter />
       </header>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,73 +1,74 @@
-import { useSelector } from "react-redux";
-import React , { useRef } from "react";
-import { useDispatch } from "react-redux";
-import { counterActions } from "../store/index";
+import { useSelector } from 'react-redux';
+import React, { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const counterDisplay = useSelector((state) => state.counter.show);
+  const inputAmountRef = useRef();
+  const isAuth = useSelector((state) => state.auth.isAuthorized);
 
-    const dispatch = useDispatch();
-    const counter = useSelector((state) => state.counter.counter);
-    const counterDisplay = useSelector((state) => state.counter.show);
-    const inputAmountRef = useRef();
+  const incrementHandler = () => {
+    // dispatch({
+    //     type: 'increment'
+    // });
+    dispatch(counterActions.increment());
+  };
+  const increaseHandler = () => {
+    dispatch(
+      counterActions.increase({
+        amount: +inputAmountRef.current.value,
+      })
+    );
+    inputAmountRef.current.value = 0;
 
-    const incrementHandler = () => {
-        // dispatch({
-        //     type: 'increment'
-        // });
-        dispatch(counterActions.increment());
-    };
-    const increaseHandler = () => {
-        dispatch(counterActions.increase({
-            amount: +inputAmountRef.current.value
-        }));
-        inputAmountRef.current.value = 0;
+    // dispatch({
+    //     type: 'increase',
+    //     amount: +inputAmountRef.current.value
+    // });
+  };
 
+  const decrementHandler = () => {
+    // dispatch({
+    //     type: 'decrement'
+    // });
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    // dispatch({
+    //     type: 'visibility'
+    // });
+    dispatch(counterActions.visibility());
+  };
+  const resetHandler = () => {
+    // dispatch({
+    //     type: 'reset'
+    // });
+    dispatch(counterActions.reset());
+    inputAmountRef.current.value = 0;
+  };
 
-        // dispatch({
-        //     type: 'increase',
-        //     amount: +inputAmountRef.current.value
-        // });
-    };
+  return (
+    <div>
+      {isAuth && <div>Welcome user.</div>}
+      {!isAuth && <div>Please login.</div>}
 
-    const decrementHandler = () => {
-        // dispatch({
-        //     type: 'decrement'
-        // });
-        dispatch(counterActions.decrement());
-    };
-    const toggleCounterHandler = () => {
-        // dispatch({
-        //     type: 'visibility'
-        // });
-        dispatch(counterActions.visibility());
-    };
-    const resetHandler = () => {
-        // dispatch({
-        //     type: 'reset'
-        // });
-        dispatch(counterActions.reset());
-        inputAmountRef.current.value = 0;
-    }
-
-
-    return (
-        <div>
-            {counterDisplay && (
-                <>
-                    <div>{counter}</div>
-                    <input type='number' ref={inputAmountRef}></input>
-                    <button onClick={increaseHandler}>Add amount</button>
-                    <button onClick={resetHandler}>Reset</button>
-                    <button onClick={incrementHandler}>+</button>
-                    <button onClick={decrementHandler}>-</button>
-                </>
-            )
-            }
-            <br />
-            <button onClick={toggleCounterHandler}>visibility</button>
-        </div>
-    )
-
-}
+      {counterDisplay && (
+        <>
+          <div>{counter}</div>
+          <input type="number" ref={inputAmountRef}></input>
+          <button onClick={increaseHandler}>Add amount</button>
+          <button onClick={resetHandler}>Reset</button>
+          <button onClick={incrementHandler}>+</button>
+          <button onClick={decrementHandler}>-</button>
+        </>
+      )}
+      <br />
+      <button onClick={toggleCounterHandler}>visibility</button>
+    </div>
+  );
+};
 
 export default Counter;
